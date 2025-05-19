3518cda72d7434dbec72f863d901c7a000200961
QuocViet <105314683+quocviet0304@users.noreply.github.com>
2025-05-19 09:00:08 +0700

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,42 @@
     $_SESSION['User_name'] = $User;
         $_SESSION['id'] = 1; 
         header('Location: index.php');
-    
+    exit;
+    //tao ket noi
+    $conn = new mysqli($host, $username, $password, $database);
+    // Kiểm tra kết nối
+    if ($conn->connect_error) {
+        die("Kết nối tới cơ sở dữ liệu thất bại: " . $conn->connect_error);
+    }
+    // else{
+    //    echo ("ket noi thanh cong");
+    // }
+
+    $User = $_POST["User"];
+    $Password = $_POST["Password"];
+
+    //trich xuat du lieu nhap bang ham 
+    $Input_user = mysqli_real_escape_string($conn, $User);
+    $Input_pass = mysqli_real_escape_string($conn, $Password);
+
+   $sql = "SELECT * FROM `account` WHERE User = '$Input_user' and Password = '$Input_pass'";
+   
+
+    $result = mysqli_query($conn, $sql);
+
+    // tắt hiển thị lỗi
+    ini_set('display_errors', '0');
+
+    $count = mysqli_num_rows($result);
+
+    if($count == 1){
+        
+        $_SESSION['User_name'] = $User;
+        $_SESSION['id'] = 1; 
+        header('Location: index.php');
+    } else{
+        header('Location: login.html');
+    }
 ?>
 
  <?php
